a60325aab5aa826b6462ab5b8cfd72293d725df2
Sumit Pandey <spandey247789@gmail.com>
2024-07-09 09:52:00 +0530

    improving reusability for star common component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const CustomStarComponent = () => {
+  const [rating, setRating] = useState(2);
+
+  return (
+    <>
+      <StarRating
+        maxRating={5}
+        messages={["Bad", "Not Bad", "Okay", "Good", "Amazing"]}
+        size={24}
+        defaultRating={rating}
+        className="test"
+        onSetRating={setRating}
+      />
+      <p style={{ fontSize: "14px", marginTop: "10px" }}>
+        This was rated {rating} stars
+      </p>
+    </>
+  );
+};
+
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <CustomStarComponent />
   </React.StrictMode>
 );
