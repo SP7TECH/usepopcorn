63b41efbfb906fca3dda52faa177080afda80b72
Sumit Pandey <spandey247789@gmail.com>
2024-10-27 20:36:35 +0530

    reverted back to original styling

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const CustomStarComponent = () => {
-  const [rating, setRating] = useState(2);
-
-  return (
-    <>
-      <StarRating
-        maxRating={5}
-        messages={["Bad", "Not Bad", "Okay", "Good", "Amazing"]}
-        size={24}
-        defaultRating={rating}
-        className="test"
-        onSetRating={setRating}
-      />
-      <p style={{ fontSize: "14px", marginTop: "10px" }}>
-        This was rated {rating} stars
-      </p>
-    </>
-  );
-};
-
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} />
-    <CustomStarComponent />
+    <App />
   </React.StrictMode>
 );
